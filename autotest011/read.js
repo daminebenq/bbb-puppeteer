38e6d105626c43254967527620b34f1ee1f261e4
damine ben salah <damine-el-mkache5@live.fr>
2019-12-16 18:11:22 +0000

    updated autotest011

--- autotest011/read.js
+++ autotest011/read.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 const URL = process.argv[2]
 const TIMELIMIT_SECONDS = parseInt(process.argv[4])
 const TIMELIMIT_MILLISECONDS = TIMELIMIT_SECONDS * 1000;
@@ -12,8 +12,7 @@ var log = function () {
 };
 async function read() {
     const browser = await puppeteer.launch({
-        headless: false,
-        executablePath: '/usr/bin/google-chrome',
+        headless: true,
 	    args: ['--no-sandbox']
     });
     // const browser = await puppeteer.connect({
